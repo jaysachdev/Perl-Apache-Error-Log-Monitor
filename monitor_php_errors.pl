f920f26ebe4e738927a889f89b5e28100bb1defd
Jay Sachdev <jay@knot-eye.com>
2011-05-10 11:12:28 -0700

    Updated to be more generic, have more error checking

--- monitor_php_errors.pl
+++ monitor_php_errors.pl
@@ -5,25 +5,39 @@ use LWP;
 use URI::Escape ('uri_escape');
 use strict;
 
-##################################################
+################################################################################
+#
+# Copyright (c) 2010, Jay Sachdev <jay@mosioproject.com>
+# (See bottom of file for license)
+#
+# Usage:
 #
 # Set the following:
 #   $file_path      : path of log to tail
 #   $email          : email to receive errors
-#   $http_error_url : URL to post to
+#   $http_error_url : URL to post to, message is escaped and appended to end.
 #
-##################################################
+################################################################################
 
 my $file_path = '';
 my $email = '';
 my $http_error_url = '';
 
+my $sendmail = '/usr/sbin/sendmail';
 
 
 # prompt for file path if not set
 if ( ! $file_path) {
 	print "\nPlease enter your error log path: ";
 	$file_path = <>;
+	chomp $file_path;
+}
+
+# prompt for email if not set
+if ( ! $email) {
+	print "\nPlease enter the email address to send errors to: ";
+	$email = <>;
+	chomp $email;
 }
 
 
@@ -31,20 +45,21 @@ if ( ! $file_path) {
 my $file = File::Tail->new( name        => $file_path,
 														debug       => 0,
 														interval    => 1,
-														maxinterval => 60,
-														adjustafter => 20,
-														errmode=>"return") or die "Could not open $file_path: $!";
+														maxinterval => 5,
+														adjustafter => 10,
+														errmode     => "die");
 
 # print some info, always nice to see the start time
 print "\n" . `date`;
 print "-- Monitoring File\n   $file_path\n\n";
 
 # main loop
-while (my $line = $file->read) {
-	if ($line =~ /php .* error/i) {
+my $line;
+while (defined($line = $file->read)) {
+	if ($line =~ /php.*error/i) {
 		print "\n--- ERROR ---\n$line\n--- /ERROR ---\n\n";
 		sendError($line);
-		smsError($line);
+		sendHttpError($line);
 	}
 }
 
@@ -54,7 +69,7 @@ exit 0;
 
 
 
-# ------------- Begin Subroutines ------------- 
+# ------------- Begin Subroutines -------------
 
 
 
@@ -65,13 +80,19 @@ exit 0;
 #
 sub sendError() {
   my $error = shift;
-  
-  if ( ! $email) {
-	  open(MAIL, '|/usr/sbin/sendmail "' . $email . '"') || (print "\n\nEMAIL NOT SENT!!!\n\n"; return 0);
+
+  if ( ! -e $sendmail) {
+  	print "Cannot find sendmail.\n";
+  	return 0;
+  }
+
+  if ($email) {
+	  open(MAIL, '| ' . $sendmail . ' "' . $email . '"') || (print "\n\nEMAIL NOT SENT!!!\n\n" && return 0);
 	  print MAIL 'To: <' . $email . '>' . "\n";
 	  print MAIL 'From: <' . $email . '>' . "\n";
 	  print MAIL "Subject: PHP Error\n\n" . $error;
 	  close(MAIL);
+	  print "Email sent to: $email\n";
   }
   else {
   	print "No email specified.\n";
@@ -81,35 +102,65 @@ sub sendError() {
 
 
 #
-# Submit error to HTTP script.  
+# Submit error to HTTP script.
 # Appends message to $http_error_url
 #
 sub sendHttpError() {
   my $error = shift;
-  
+
   if ( ! $http_error_url) {
   	print "No HTTP URL specified.\n";
   	return 1;
   }
-  
+
 	# escape & append error
-	my $get_url = $http_error_url . uri_escape($error); 
+	my $get_url = $http_error_url . uri_escape($error);
 
   # send using LWP
   my $ua = LWP::UserAgent->new;
   my $res = $ua->request(HTTP::Request->new(GET => $get_url));
-  
+
   ##
   ## Uncomment if you want to see the content of this GET request
   ##
-#  print "\n------- smsError HTTP Response -------\n";
+#  print "\n------- sendHttpError HTTP Response -------\n";
 #	if ($res->is_success) {
 #		print $res->content . "\n";
 #	}
 #	else {
 #		print $res->status_line, "\n";
 #	}
-#  print "\n------- /smsError HTTP Response -------\n";
-	
+#  print "\n------- /sendHttpError HTTP Response -------\n";
+
 	return $res->is_success;
 }
+
+
+
+=pod
+
+
+Copyright (C) 2010 by Jay Sachdev <jay@mosioproject.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a 
+copy of this software and associated documentation files (the 
+"Software"), to deal in the Software without restriction, including 
+without limitation the rights to use, copy, modify, merge, publish, 
+distribute, sublicense, and/or sell copies of the Software, and to 
+permit persons to whom the Software is furnished to do so, subject to 
+the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+=cut
